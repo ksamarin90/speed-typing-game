6bf9e846eda8e07e06a170a404e5a63f9c2387cb
ksamarin <ksamarin90@gmail.com>
2020-03-27 17:15:52 +0300

    Updates

---
+++ precache-manifest.8e0986c2a74061ef05eebd5195af6d08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "149610b2ab3f1a8d2029dded1e1436da",
+    "url": "/speed-typing-game/index.html"
+  },
+  {
+    "revision": "b835a66d974406be792d",
+    "url": "/speed-typing-game/static/js/2.fa214371.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/speed-typing-game/static/js/2.fa214371.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5f8e2139126937384fb",
+    "url": "/speed-typing-game/static/js/main.b3f89c84.chunk.js"
+  },
+  {
+    "revision": "61843d337aeecaaa5dec",
+    "url": "/speed-typing-game/static/js/runtime-main.9c72eda6.js"
+  }
+]);
